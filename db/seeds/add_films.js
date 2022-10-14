abe47576100330e6a9bfc02422bb03f40f0ee3eb
Ivan-juni <ivanuk.vanya@gmail.com>
2022-10-14 15:17:01 +0300

    batch update

--- db/seeds/add_films.js
+++ db/seeds/add_films.js
@@ -1,11 +1,12 @@
 const faker = require("@faker-js/faker");
 
-const FILMS = [];
-
-function createRandomFilm(filmNumber) {
-  filmNumber++;
+// generate new film with faker
+function createRandomFilm() {
   return {
-    name: `Film ${faker.faker.datatype.number}`,
+    name: `Film ${faker.faker.datatype.number({
+      min: 1,
+      max: 1000000,
+    })}`,
     year: faker.faker.datatype.number({
       min: 1990,
       max: 2022,
@@ -17,37 +18,41 @@ function createRandomFilm(filmNumber) {
   };
 }
 
-// seed
+//seed
 exports.seed = function (knex) {
-  // Deletes ALL existing entries
-  return knex("films")
-    .del()
-    .then(function () {
-      const insertMFilms = () => {
-        Array.from({ length: 1000000 }).forEach(() => {
-          FILMS.push(createRandomFilm());
-        });
-        return FILMS;
-      };
-      // Inserts seed entries
-      return knex.batchInsert("films", insertMFilms(), 5000);
-    });
+  if (false) {
+    // push generated films to insert
+    function pushFilms() {
+      const FILMS = [];
+      Array.from({ length: 1000000 }).forEach(() => {
+        FILMS.push(createRandomFilm());
+      });
+      return FILMS;
+    }
+    return (
+      knex("films")
+        // Deletes ALL existing entries
+        .del()
+        .then(function () {
+          const insertFilms = () => {
+            return pushFilms();
+          };
+          // Inserts seed entries
+          return knex.batchInsert("films", insertFilms(), 5000);
+        })
+    );
+    //   return knex("films")
+    //     .del()
+    //     .then(function () {
+    //       const insertMFilms = () => {
+    //         const films = [];
+    //         for (let i = 1; i <= 1000000; i++) {
+    //           films.push({ name: `Фильм ${i}`, year: 2010, id_country: 1 });
+    //         }
+    //         return films;
+    //       };
+    //       // Inserts seed entries
+    //       return knex.batchInsert("films", insertMFilms(), 5000);
+    //     });
+  }
 };
-
-// // seed
-// exports.seed = function (knex) {
-//   // Deletes ALL existing entries
-//   return knex("films")
-//     .del()
-//     .then(function () {
-//       const insertMFilms = () => {
-//         const films = [];
-//         for (let i = 1; i <= 1000000; i++) {
-//           films.push({ name: `Фильм ${i}`, year: 2010, id_country: 1 });
-//         }
-//         return films;
-//       };
-//       // Inserts seed entries
-//       return knex.batchInsert("films", insertMFilms(), 5000);
-//     });
-// };
---
+++ db/seeds/upd_films.js
@@ -0,0 +1,57 @@
+const faker = require("@faker-js/faker");
+
+// generate new film with faker
+function createRandomFilm() {
+  return {
+    name: `Film ${faker.faker.datatype.number({
+      min: 1,
+      max: 1000000,
+    })}`,
+    year: faker.faker.datatype.number({
+      min: 1990,
+      max: 2022,
+    }),
+    id_country: faker.faker.datatype.number({
+      min: 1,
+      max: 4,
+    }),
+  };
+}
+
+// add id to this filmsupdate
+exports.seed = function (knex) {
+  if (true) {
+    // push generated films to update
+    function pushFilms() {
+      const FILMS = [];
+      Array.from({ length: 10000 }).forEach(() => {
+        FILMS.push(createRandomFilm());
+      });
+      return FILMS;
+    }
+
+    const films = pushFilms();
+    for (let i = 0; i < films.length; i++) {
+      const film = films[i];
+      film.id = i + 1;
+    }
+    return knex.transaction((trx) => {
+      const queries = [];
+      films.forEach((film) => {
+        const query = knex("films")
+          .where("id", film.id)
+          .update({
+            name: film.name,
+            year: film.year,
+            id_country: film.id_country,
+          })
+          .transacting(trx); // This makes every update be in the same transaction
+        queries.push(query);
+      });
+
+      Promise.all(queries) // Once every query is written
+        .then(trx.commit) // We try to execute all of them
+        .catch(trx.rollback); // And rollback in case any of them goes wrong
+    });
+  }
+};
